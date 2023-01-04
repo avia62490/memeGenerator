8ffb494f5f10793e440fec71b8bb254518d364d3
Alex Via <alexandervia@gmail.com>
2023-01-04 15:45:02 -0600

    adds input component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header.js'
+import Input from './components/Input.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Input />
     </div>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,13 @@
+export default function Input() {
+    return(
+        <div className="input">
+            <div className="input--text">
+                <input type="text" />
+                <input type="text" />
+            </div>
+            <div className="input--submit">
+                <input type="submit" />
+            </div>
+        </div>
+    )
+}
