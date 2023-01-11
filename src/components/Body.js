01e82db397b94adda6ea419718e52a74bdf3a948
Alex Via <alexandervia@gmail.com>
2023-01-11 12:11:13 -0600

    handles event change, designates top and bottom text

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,15 +2,31 @@ import memesData from '../memesData.js'
 import React from 'react'
 
 export default function Input() {
-    const [meme, setMeme] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        image:"http://i.imgflip.com/1bij.jpg"
+    })
+
     let memeURL
     const memesArray = memesData.data
+    
     function getMeme() {
         const number= Math.floor(Math.random() * memesArray.length);
         memeURL = memesArray[number].url
-        setMeme(memeURL)
-        console.log(`getting meme ${memeURL}`)
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            image : memeURL
+        }))
     }
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    console.log(meme)
 
     return(
         <div className="body">
@@ -19,15 +35,25 @@ export default function Input() {
                     className="input--field" 
                     type="text" 
                     placeholder="Top text"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input
                     className="input--field" 
                     type="text" 
                     placeholder="Bottom text"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button onClick={getMeme} className="input--button">Get a new meme image  🖼</button>
             </div>
-            <img src={meme} alt=''className='meme'/>
+            <div className="meme">
+                <img src={meme.image} className="meme--image" alt=''/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </div>
     )
 }
